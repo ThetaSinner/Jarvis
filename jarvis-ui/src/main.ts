96717b2925854416dd659918c3eb1034f584af3c
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-11-15 00:27:29 +0000

    Add typescript support

--- jarvis-ui/src/main.ts
+++ jarvis-ui/src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Type errors on the provided code?
+// @ts-ignore
 createApp(App).use(router).mount('#app')
